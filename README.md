a3906e4c7a2eee6d109593e610d68bd08130f194
chendaxixi <215bcsb342@gmail.com>
2015-11-10 17:26:03 +0800

    add README.md

--- README.md
+++ README.md
@@ -10,7 +10,13 @@
 ###Demo使用说明
 
 1. 在wrist_nodejs文件夹下调用npm install即可
-2. 请在wrist_nodejs/routes/tools.js内补充
+2. 请在wrist_nodejs/routes/tools.js内补充以下信息:
+	1. token
+	2. appid
+	3. appsrc
+3. wrist_nodejs/routes/m_wechat.js下为各消息、推送事件的处理
+4. wrist_nodejs/routes/tools.js内为主动调用的API
+5. 用于微信测试号：将app.js的端口(app.listen)改成80端口，或者用Nginx、Apache将80端口转发到10024端口
 
 ###Wechat 主动调用API
 
--- wrist_nodejs/routes/test.js
+++
@@ -1,9 +0,0 @@
-var tools = require('./tools');
-
-var user = "ose6Ut8Ir-41wB7gQx89BifYa49Q";
-var user_2 = "ose6UtzLl9NJj7DP7AZAOOybiH1k";
-
-//tools.customSendArticle(user_2, "信仰圣光吧", "圣光会制裁你的！", "http://image.baidu.com/search/down?tn=download&ipn=dwnl&word=download&ie=utf8&fr=result&url=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fblog%2F201403%2F23%2F20140323004621_TX2yr.jpeg", "http://print.ssast2015.com", function(err, result){console.log(result);});
-//tools.getStat("gh_99e1bf01488e_001", function(err, result){console.log(result);});
-
-tools.createQrCode("gh_99e1bf01488e_001", function(err, result){console.log(err);console.log(result);});
